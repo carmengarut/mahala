97a64ca4331c99c4270968b96770fdf4380c8b68
carmegarut <carmencgu3@gmail.com>
2022-02-16 16:39:12 +0100

    add lazy carousel component

---
+++ src/components/Carousel.js
@@ -0,0 +1,59 @@
+import Carousel from 'react-bootstrap/Carousel'
+
+import etrust1 from './public/etrust1.svg'
+import etrust2 from './public/etrust2.svg'
+import etrust3 from './public/etrust3.svg'
+import kampei1 from './public/kampei1.svg'
+import kampei2 from './public/kampei2.svg'
+import kampei3 from './public/kampei3.svg'
+
+export default function CarouselMobile () {
+    return (
+        <Carousel variant='dark' className='lp-carousel'>
+          <Carousel.Item>
+            <img
+              className="d-block w-100 lp-carousel-item"
+              src={etrust1}
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100 lp-carousel-item"
+              src={etrust2}
+              alt="Second slide"  
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100 lp-carousel-item"
+              src={etrust3}
+              alt="Third slide"   
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100 lp-carousel-item"
+              src={kampei1}
+              alt="Fourth slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100 lp-carousel-item"
+              src={kampei2}
+              alt="Fifth slide"
+            
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100 lp-carousel-item"
+              src={kampei3}
+              alt="Sixth slide"
+           
+            />
+          </Carousel.Item>
+        </Carousel>
+    )
+}
